f43f75cd54c0e422f83108764df7027c58b09bfb
anktsngh48 <anktsngh48@gmail.com>
2026-03-02 17:33:34 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,10 +1,10 @@
-const APP_SCRIPT_URL = 'https://script.google.com/macros/s/AKfycbxOl7D232cMb3ZPW2GrKco_m7Cxz_cToR_BbOMffxPp-GqrVRkGOX7X33CqpIjuUZ475w/exec'; // Replace with your Web App URL
+const APP_SCRIPT_URL = 'https://script.google.com/macros/s/AKfycbxOl7D232cMb3ZPW2GrKco_m7Cxz_cToR_BbOMffxPp-GqrVRkGOX7X33CqpIjuUZ475w/exec';  // Replace with your Web App URL
 
 async function loadDoctors() {
-    const response = await fetch(`${APP_SCRIPT_URL}/getDoctors`);
+    const response = await fetch(`${APP_SCRIPT_URL}?action=getDoctors`);
     const doctors = await response.json();
     let doctorDropdown = document.getElementById('doctorDropdown');
-    
+
     if (doctors.length > 1) {
         doctorDropdown.innerHTML = `<select onchange="loadDoctorImage(this.value)">${doctors.map(doctor => `<option value="${doctor}">${doctor}</option>`).join('')}</select>`;
     } else {
@@ -14,14 +14,14 @@ async function loadDoctors() {
 }
 
 async function loadClinics() {
-    const response = await fetch(`${APP_SCRIPT_URL}/getClinics`);
+    const response = await fetch(`${APP_SCRIPT_URL}?action=getClinics`);
     const clinics = await response.json();
     let clinicDropdown = document.getElementById('clinicDropdown');
     clinicDropdown.innerHTML = `<select onchange="updateAddress(this.value)">${clinics.map(clinic => `<option value="${clinic}">${clinic}</option>`).join('')}</select>`;
 }
 
 async function updateAddress(selectedClinic) {
-    const response = await fetch(`${APP_SCRIPT_URL}/getAddress?clinicName=${selectedClinic}`);
-    const address = await response.text(); // Assuming the return value is a text address
+    const response = await fetch(`${APP_SCRIPT_URL}?action=getAddress&clinicName=${selectedClinic}`);
+    const address = await response.text(); 
     document.getElementById('addressDisplay').innerText = address;
 }
